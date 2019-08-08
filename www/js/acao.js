98082d19554e4361c6364371613d4e994efe0e2e
yvolorenzo2019 <yvolorezo@gmail.com>
2019-08-09 08:08:24 +0900

    Criaçao da pagina de pos login , alteraçoes de estilo, novas funcoes , redirecionamentos, .

--- www/js/acao.js
+++ www/js/acao.js
@@ -1,5 +1,12 @@
-// This is a JavaScript file
+// Redirecionamentos 
 function poslogin() {
+  location.href = "poslogin.html";
+}
+function itensCarrinho(){
+  location.href = "itensCarrinho.html";
+}
+// Funções pagina carrinho
+function fechar(){
   location.href = "poslogin.html";
 }
 
