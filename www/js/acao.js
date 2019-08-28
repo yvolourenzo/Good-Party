1a8da7c287f42cacbfd4ff1cf3e969b09ec685eb
yvolorenzo2019 <yvolorezo@gmail.com>
2019-08-29 05:24:17 +0900

    Tela de cadastro breadcrumb

--- www/js/acao.js
+++ www/js/acao.js
@@ -30,3 +30,29 @@ function fechar() {
 function bolococo(){
   location.href = "bolococo.html";
 }
+function myFunction() {
+  var y = document.getElementById("Div1");
+  var x = document.getElementById("Div2");
+  var z = document.getElementById("Div3");
+  if (x.style.display === "none" && y.style.display == "none") {
+    x.style.display = "block";
+  }
+   else {
+    x.style.display = "none";
+  }
+}
+// Tratamento de Senha
+var password = document.getElementById("senha")
+  , confirm_password = document.getElementById("senhac");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Senhas diferentes!");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
