d18c8a42e5a08596d6e1235ef79fceb271f4be34
yvolorenzo2019 <47569750+yvolorenzo2019@users.noreply.github.com>
2019-10-10 04:52:03 +0900

    Adicionado o Upload de foto \( Cadastro \)

--- www/js/comandosParaObanco.js
+++ www/js/comandosParaObanco.js
@@ -1,14 +1,14 @@
 // Comando para salvar o Cadastro
 $(document).on("click","#finalizar",function(){
- // var prop = document.getElementById('caminho').files[0];
- // var nome_imagem = prop.name;
-  //var extensao_imagem = nome_imagem.split('.').pop().toLowerCase();
+   var prop = document.getElementById('caminho').files[0];
+   var nome_imagem = prop.name;
+   var extensao_imagem = nome_imagem.split('.').pop().toLowerCase();
 
-  /*if(jQuery.inArray(extensao_imagem,['png','jpg','jpeg']) == -1) {
+   if(jQuery.inArray(extensao_imagem,['png','jpg','jpeg']) == -1) {
      navigator.notification.alert("imagem invalida");
-  }else{*/
+  }else{ 
     var form_data = new FormData();
-    // form_data.append("foto",prop);
+     form_data.append("foto",prop);
      form_data.append("nome",$("#nome").val());
      form_data.append("senha",$("#senha").val());
      form_data.append("email",$("#email").val());
@@ -27,9 +27,9 @@ $(document).on("click","#finalizar",function(){
         cache:false,
         processData:false,
         success:function(data){
-          alert(data);
+          alert("Muito obrigado por se registrar!!");
           location.reload(); 
          }
      });
-  //}
+   }
 });
