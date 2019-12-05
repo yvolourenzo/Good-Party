af25e8d78f4f56d8548ba898283908b9197d1e48
yvolorenzo2019 <yvolorenzo@gmail.com>
2019-12-06 01:29:18 +0900

    Melhorado array da pesquisa , Arrumando botão de editar informações , Arquivos desnecessários deletados

--- www/js/listarProdutoVendedor.js
+++ www/js/listarProdutoVendedor.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 				//Adicionando registros retornados na tabela
          itemProduto +=
   
-        "<div class='col-xs-12 linha itemProd' data-toggle='modal' data-target='#modalProduto' data-id='" + dados.codigo + "'><img class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto + "' style=' width: 100%; height: 200px; margin-top:10px; border-radius: 10px 10px 0px 0px;'><input id='nome' style='background-color:white; text-align:center; font-size:20px; width:100%;'  placeholder='" + dados.produto + "' readonly><input id='valor' style='background-color:white; text-align:center; font-size:20px; width:100%;'  placeholder='Valor: " + dados.valor + "' readonly><input id='quantidade' style='background-color:white; text-align:center; font-size:20px; width:100%;'  placeholder='Qntd: " + dados.quantidade + "' readonly><textarea style='background-color:white; text-align:center; font-size:20px; width:100%;'  placeholder='Descrição: " + dados.descricao + "' readonly></textarea><input class='form-control impu display' type='file' name='caminho' id='caminho' ></p><button class='botao' style='widht:50%;' id='deletar' onclick='deletar("+dados.codigo+")'>Excluir</button><button class='botao' style='widht:50%;' id='alterar' onclick='alterar("+dados.codigo+")'>Alterar</button></div></div></div></div><style>.display{display:none;}.displayB{display: block;}</style>" 
+        "<div class='col-xs-12 linha itemProd' data-toggle='modal' data-target='#modalProduto' data-id='" + dados.codigo + "'><img class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto + "' style=' width: 100%; height: 200px; margin-top:10px; border-radius: 10px 10px 0px 0px;'><input id='nome' style='background-color:white; text-align:center; font-size:20px; width:100%;'  placeholder='" + dados.produto + "' readonly><input id='valor' style='background-color:white; text-align:center; font-size:20px; width:100%;'  placeholder='Valor: " + dados.valor + "' readonly><input id='quantidade' style='background-color:white; text-align:center; font-size:20px; width:100%;'  placeholder='Qntd: " + dados.quantidade + "' readonly><textarea style='background-color:white; text-align:center; font-size:20px; width:100%;'  placeholder='Descrição: " + dados.descricao + "' readonly></textarea></p><!--                                                                                                                                                     BOTAO                                                                --><button class='botao' style='widht:50%;' id='deletar' onclick='deletar("+dados.codigo+")'>Excluir</button><button class='botao' style='widht:50%;' id='alterar' href='#janela"+dados.codigo+"' rel='modal"+dados.codigo+"'>Alterar</button></div><style>.display{display:none;}.displayB{display: block;}</style>                                                                                                            <!--  JANELA MODAL-->                                  <div class='window' id='janela"+dados.codigo+"'><a href='#' class='fechar'>X Fechar</a><h4 style='text-align:center;'>Alterar Produto</h4><img class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto + "'><p style='text-align:center;'>Nome</p><input placeholder='"+dados.produto+"' class='form-control' id='nomeAlterado"+dados.codigo+"'><p style='text-align:center;'>Valor</p><input placeholder='"+dados.valor+"' class='form-control' id='valorAlterado"+dados.codigo+"'><button id='salvar' class='botao' onclick='salvarAlteracoes("+dados.codigo+")'>Salvar</button></div><div id='mascara'></div><script>$(document).ready(function(){$('button[rel=modal"+dados.codigo+"]').click( function(ev){ev.preventDefault();var id = $(this).attr('href');var alturaTela = $(document).height();var larguraTela = $(window).width();$('#mascara').css({'width':larguraTela,'height':alturaTela});$('#mascara').fadeIn(1000); $('#mascara').fadeTo('slow',0.8);var left = ($(window).width() /2) - ( $(id).width() / 2 );var top = ($(window).height() / 2) - ( $(id).height() / 2 );$(id).css({'top':top,'left':left});$(id).show();});$('#mascara').click( function(){$(this).hide();$('.window').hide();});$('.fechar').click(function(ev){ev.preventDefault();$('#mascara').hide();$('.window').hide();});}); function salvarAlteracoes(arg){var form_data = new FormData();form_data.append('codigo',arg);form_data.append('nome',$('#nomeAlterado"+dados.codigo+"').val());form_data.append('valor',$('#valorAlterado"+dados.codigo+"').val());$.ajax({url:'https://acheulean-limps.000webhostapp.com/alteraProduto.php',method:'POST',data:form_data,contentType:false,cache:false,processData:false,success:function(dados){alert('Dados atualizados com sucesso!');location.reload();},error : function (request, status, error){alert(request.responseText);}});};</script>" 
 
       });
       $("#tabela").html(itemProduto);
@@ -41,20 +41,7 @@ function deletar(arg) {
      });
 };
 
-$('#alterar').click(function(){
-      $("#nome").prop("readonly", false);
-      alert('meu rabo');
-  desabilita();
 
-});
-function desabilita() {
-  $("#nome").prop("readonly", true);
-  $("#valor").prop("readonly", true);
-  $("#quantidade").prop("readonly", true);
-  $("#descro").prop("readonly", true);
-   
-   
-}
 
 
 
--- www/js/pesquisa.js
+++ www/js/pesquisa.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 				//Adicionando registros retornados na tabela
          itemProduto +=
   
-        "<div class='col-xs-12 linha itemProd' data-toggle='modal' data-target='#modalProduto' data-id='" + dados.codigo + "'><img class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto + "'><p id='nome' style='background-color:white; text-align:center; font-size:20px; width:100%;'>" + dados.produto + "<br> R$:" + dados.valor + " a/o " + dados.quantidade + " <br>  Descrição:" + dados.descricao + "</p></p><a href='#janela"+dados.codigo+"' rel='modal"+dados.codigo+"'>Solicitar</a></div><!--                                                                                                                                          JANELA MODAL-->                                  <div class='window' id='janela"+dados.codigo+"'><a href='#' class='fechar'>X Fechar</a><h4 style='text-align:center;'>"+dados.produto+"</h4><img class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto + "'><p style='font-size:18px;'>"+dados.produto+" esta por apenas R$:" + dados.valor +" a/o " + dados.quantidade + " </p><p style='text-align:center; font-size:18px;'>Quantos deseja solicitar ?</p><input class='form-control' id='quantidade' placeholder='Digite a quantidade aqui'></div><div id='mascara'></div><script>$(document).ready(function(){$('a[rel=modal"+dados.codigo+"]').click( function(ev){ev.preventDefault();var id = $(this).attr('href');var alturaTela = $(document).height();var larguraTela = $(window).width();$('#mascara').css({'width':larguraTela,'height':alturaTela});$('#mascara').fadeIn(1000); $('#mascara').fadeTo('slow',0.8);var left = ($(window).width() /2) - ( $(id).width() / 2 );var top = ($(window).height() / 2) - ( $(id).height() / 2 );$(id).css({'top':top,'left':left});$(id).show();});$('#mascara').click( function(){$(this).hide();$('.window').hide();});$('.fechar').click(function(ev){ev.preventDefault();$('#mascara').hide();$('.window').hide();});});</script>" 
+        "<div class='col-xs-12 linha itemProd' data-toggle='modal' data-target='#modalProduto' data-id='" + dados.codigo + "'><img class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto + "'><p id='nome' style='background-color:white; text-align:center; font-size:20px; width:100%;'>" + dados.produto + "<br> R$:" + dados.valor + " a/o " + dados.quantidade + " <br>  Descrição:" + dados.descricao + "</p></p><button class='botao' href='#janela"+dados.codigo+"' rel='modal"+dados.codigo+"'>Solicitar</button></div><!--                                                                                                                                          JANELA MODAL-->                                  <div class='window' id='janela"+dados.codigo+"'><a href='#' class='fechar'>X Fechar</a><h4 style='text-align:center;'>"+dados.produto+"</h4><img class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto + "'><div class='vendedor' style='background-color:#ECECEC'><h4 style='text-align:center;'>Vendedor</h4><p style='text-align:center; font-size:18px;'>" + dados.usuario +"</p><img style='border-radius:90px; height:50px; margin-left:45%;' class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto_usuario + "'><p style='font-size:18px; text-align:center;'>" + dados.sg_estado +" ," + dados.endereco +" </p></div>"+dados.produto+" esta por apenas R$:" + dados.valor +" a/o " + dados.quantidade + " </p><p style='text-align:center; font-size:18px;'>Quantos deseja solicitar ?</p><input class='form-control' id='quantidade' placeholder='Digite a quantidade aqui' ><button class='botao' onclick='solicitar("+dados.codigo+")'>Solicitar</button></div><div id='mascara'></div><script>$(document).ready(function(){$('button[rel=modal"+dados.codigo+"]').click( function(ev){ev.preventDefault();var id = $(this).attr('href');var alturaTela = $(document).height();var larguraTela = $(window).width();$('#mascara').css({'width':larguraTela,'height':alturaTela});$('#mascara').fadeIn(1000); $('#mascara').fadeTo('slow',0.8);var left = ($(window).width() /2) - ( $(id).width() / 2 );var top = ($(window).height() / 2) - ( $(id).height() / 2 );$(id).css({'top':top,'left':left});$(id).show();});$('#mascara').click( function(){$(this).hide();$('.window').hide();});$('.fechar').click(function(ev){ev.preventDefault();$('#mascara').hide();$('.window').hide();});});</script>" 
 
 
          
@@ -22,6 +22,30 @@ $(document).ready(function(){
     },
 	});  
 });
+
+function solicitar(arg){  
+    var form_data = new FormData();
+     form_data.append("id_produto",arg);
+      form_data.append("quantidade",$("#quantidade").val());
+
+        $.ajax({
+        url:"https://acheulean-limps.000webhostapp.com/enviarSolicitacao.php",
+        method:'POST',
+        data:form_data,
+        contentType:false,
+        cache:false,
+        processData:false,
+        success:function(dados){
+             
+          alert('Sua solicitação foi enviada!');
+
+          location.reload();
+         },
+         error : function (request, status, error) {
+           alert(request.responseText);
+         }
+     });
+};
 
 
 $(document).on("click","#pesquisar",function(){
@@ -45,7 +69,7 @@ $(document).on("click","#pesquisar",function(){
 				//Adicionando registros retornados na tabela
          itemProduto +=
   
-        "<div class='col-xs-12 linha itemProd' data-toggle='modal' data-target='#modalProduto' data-id='" + dados.codigo + "'><img class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto + "' style=' width: 100%; height: 200px; margin-top:10px; border-radius: 10px 10px 0px 0px;'><p id='nome' style='background-color:white; text-align:center; font-size:20px; width:100%;'>" + dados.produto + "<br> R$:" + dados.valor + " a/o " + dados.quantidade + " <br>  Descrição:" + dados.descricao + "</p></p><button class='botao'><a class='botao'  href='#janela1' rel='"+dados.codigo+"'>Solicitar</a></button><div class='window' id='janela1'><a href='#' class='fechar'>X</a><h4>Digite a quantidade que deseja solicitar ao vendedor Este produto esta sendo vendido R$:" + dados.valor + " a/o " + dados.quantidade + " </h4><input class='form-control' id='quantidade' type='number'><br><button class='botao' style='widht:50%;' id='concluir' onclick='concluir("+dados.codigo+")'>Concluir</button></div></div></div></div></div>" 
+        "<div class='col-xs-12 linha itemProd' data-toggle='modal' data-target='#modalProduto' data-id='" + dados.codigo + "'><img class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto + "'><p id='nome' style='background-color:white; text-align:center; font-size:20px; width:100%;'>" + dados.produto + "<br> R$:" + dados.valor + " a/o " + dados.quantidade + " <br>  Descrição:" + dados.descricao + "</p></p><button class='botao' href='#janela"+dados.codigo+"' rel='modal"+dados.codigo+"'>Solicitar</button></div><!--                                                                                                                                          JANELA MODAL-->                                  <div class='window' id='janela"+dados.codigo+"'><a href='#' class='fechar'>X Fechar</a><h4 style='text-align:center;'>"+dados.produto+"</h4><img class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto + "'><div class='vendedor' style='background-color:#ECECEC'><h4 style='text-align:center;'>Vendedor</h4><p style='text-align:center; font-size:18px;'>" + dados.usuario +"</p><img style='border-radius:90px; height:50px; margin-left:45%;' class='img-responsive' src='https://acheulean-limps.000webhostapp.com/" + dados.foto_usuario + "'><p style='font-size:18px; text-align:center;'>" + dados.sg_estado +" ," + dados.endereco +" </p></div>"+dados.produto+" esta por apenas R$:" + dados.valor +" a/o " + dados.quantidade + " </p><p style='text-align:center; font-size:18px;'>Quantos deseja solicitar ?</p><input class='form-control' id='quantidade' placeholder='Digite a quantidade aqui' ><button class='botao' onclick='solicitar("+dados.codigo+")'>Solicitar</button></div><div id='mascara'></div><script>$(document).ready(function(){$('button[rel=modal"+dados.codigo+"]').click( function(ev){ev.preventDefault();var id = $(this).attr('href');var alturaTela = $(document).height();var larguraTela = $(window).width();$('#mascara').css({'width':larguraTela,'height':alturaTela});$('#mascara').fadeIn(1000); $('#mascara').fadeTo('slow',0.8);var left = ($(window).width() /2) - ( $(id).width() / 2 );var top = ($(window).height() / 2) - ( $(id).height() / 2 );$(id).css({'top':top,'left':left});$(id).show();});$('#mascara').click( function(){$(this).hide();$('.window').hide();});$('.fechar').click(function(ev){ev.preventDefault();$('#mascara').hide();$('.window').hide();});});</script>" 
 
       });
       $("#tabela").html(itemProduto);
